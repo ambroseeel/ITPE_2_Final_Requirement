de043c26e6f2c6d43441e18c84d0c28fa42699a4
Jasmine Faye Andres <invaliduser3636@gmail.com>
2023-12-08 13:08:24 +0800

    base design

--- index.js
+++ index.js
@@ -81,3 +81,23 @@ function login(email, password) {
   // Success validation
   alert(`Welcome ${users[userIndex].name}.`);
 }
+
+
+// for registration form
+
+(function () {
+  'use strict'
+  const forms = document.querySelectorAll('.requires-validation')
+  Array.from(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+  
